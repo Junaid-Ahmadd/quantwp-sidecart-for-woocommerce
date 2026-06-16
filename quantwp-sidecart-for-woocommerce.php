9d24c18b9fe924063e2ac9f1a81a410c30cf24af
junaid-ahmadd <malikjunaidahmad007@gmail.com>
2026-06-17 04:31:01 +0500

    Added woocommerce HPOS compatibility code

--- quantwp-sidecart-for-woocommerce.php
+++ quantwp-sidecart-for-woocommerce.php
@@ -22,6 +22,15 @@ if (!defined('ABSPATH')) {
 }
 
 
+/**
+ * Declare compatibility with WooCommerce High-Performance Order Storage (HPOS)
+ */
+add_action('before_woocommerce_init', function() {
+    if (class_exists(\Automattic\WooCommerce\Utilities\FeaturesUtil::class)) {
+        \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility('custom_order_tables', __FILE__, true);
+    }
+});
+
 // Define plugin constants
 define('QUANTWP_VERSION', '4.1.2');
 define('QUANTWP_PATH', plugin_dir_path(__FILE__));
